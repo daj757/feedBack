2aee9045e54927f0e58543d040d005936cd43979
Danny <DJ@MacBook-Pro.local>
2018-04-10 15:24:00 -0600

    auth added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+require("./server/models/User");
 require("./server/services/passport");
 const PORT = process.env.PORT || 3000;
 const keys = require("./server/config/keys");
 require("./server/routes/auth-routes")(app);
 
 mongoose.connect(keys.mongoURI);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.listen(PORT);
